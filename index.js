22c2b188caaf43f8913c1ca792509077b14767c3
Josh Brown <joshuaebrown021@gmail.com>
2022-03-25 21:46:31 -0400

    Adding places view with mock data(not working directions in part 4 was not clear)

--- index.js
+++ index.js
@@ -15,4 +15,22 @@ app.get('*', (req, res) => {
     res.status(404).render('error404')
 })
 
+// GET /places
+app.get('/', (req, res) => {
+    let places = [{
+        name: 'H-Thai-ML',
+        city: 'Seattle',
+        state: 'WA',
+        cuisines: 'Thai, Pan-Asian',
+        pic: 'http://placekitten.com/250/250'
+      }, {
+        name: 'Coding Cat Cafe',
+        city: 'Phoenix',
+        state: 'AZ',
+        cuisines: 'Coffee, Bakery',
+        pic: 'http://placekitten.com/250/250'
+      }]
+      res.render('places/index', { places })
+  })
+
 app.listen(process.env.PORT)
