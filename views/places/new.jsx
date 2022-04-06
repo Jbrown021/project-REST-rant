b738dbed7d1da123937dcb3a9801b455d9882398
Josh Brown <joshuaebrown021@gmail.com>
2022-04-06 19:53:59 -0400

    updated activity 6.8.8

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -12,9 +12,14 @@ function new_form () {
     <label htmlFor="name">Place Name</label>
     <input className="form-control" id="name" name="name" required />
   </div>
-  <div className="form-group">
-    <label for="founded">Founded Year</label>
-    <input className="form-control" id="founded" name="founded" />
+  <div className="form-group col-sm-4">
+    <label htmlFor="founded">Founded Year</label>
+    <input 
+      type="number"
+      className="form-control"
+      id="founded" 
+      name="founded"
+      value={new Date().getFullYear()} />
 </div>
   <div className="form-group">
     <label htmlFor="pic">Place Picture</label>
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -3,8 +3,17 @@ const Def = require('../default')
 
 function show (data) {
     return (
-        <Def>
-          <main>
+ <Def>
+  <main>
+    <div className="row"/>
+      <div className="col-sm-6">
+        <img src={data.place.pic} alt={data.place.name} />
+        <h3>
+          Located in {data.place.city}, {data.place.state}
+        </h3>
+      </div>
+      <div className="col-sm-6">
+      <main>
           <h1>{data.place.name}</h1>
 				<a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
   				Edit   
@@ -21,14 +30,23 @@ function show (data) {
             
             </form>     
           </main>
-        </Def>
+        <h2>
+          Description
+        </h2>
+        <h3>
+          {data.place.showEstablished()}
+        </h3>
+        <h4>
+          Serving {data.place.cuisines}
+        </h4>
+        ...
+    </div>
+  </main>
+</Def>
     )
 }
 
-// Ratings
 
-
-//comments
 
 
 
