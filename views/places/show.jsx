31728f1c30d35f691734ff6a8fcd75086a5a1aa4
Josh Brown <joshuaebrown021@gmail.com>
2022-04-08 21:35:02 -0400

    added comments

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,6 +2,25 @@ const React = require('react')
 const Def = require('../default')
 
 function show (data) {
+  let comments = (
+    <h3 className="inactive">
+      No comments yet!
+    </h3>
+  )
+  if (data.place.comments.length) {
+    comments = data.place.comments.map(c => {
+      return (
+        <div className="border">
+          <h2 className="rant">{c.rant ? 'Rant! 😡' : 'Rave! 😻'}</h2>
+          <h4>{c.content}</h4>
+          <h3>
+            <stong>- {c.author}</stong>
+          </h3>
+          <h4>Rating: {c.stars}</h4>
+        </div>
+      )
+    })
+  }
     return (
  <Def>
   <main>
@@ -25,6 +44,7 @@ function show (data) {
             <h2>Rating</h2>
             <p>Not Rated</p>
             <h2>Comments</h2>
+            {comments}
             <p>No comments yet! </p>
 
             
